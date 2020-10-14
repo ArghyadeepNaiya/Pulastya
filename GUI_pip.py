ac86f62dcf5bd9d9ea044948e2da7b17855967bf
ArghyadeepNaiya <69076321+ArghyadeepNaiya@users.noreply.github.com>
2020-10-14 18:55:24 +0530

    Create GUI_pip.py

---
+++ GUI_pip.py
@@ -0,0 +1,19 @@
+from pip import main as I
+from tkinter import *
+
+def install():     
+    print('================AM PIP FOR ALL ====================')
+    x=input('Enter the module name you wanna install: ')
+    I(['install' ,x])
+
+"""____________________________________________________________
+Oi tuklu ekta GUI design kor niche ar Tate button er command 
+Ta oi install function ta call koris thhik achhe 
+Hello oi............
+Ar eita hobe ekta GUI pip installer chol suru 
+Kor kaj joldi plsss 
+🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏
+##################
+
+________________________________________________________________
+"""
